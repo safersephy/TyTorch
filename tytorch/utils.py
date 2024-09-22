f55956271e0895e7b227276314b3d279e6895cd7
SaferSephy <t.vandervelden3@student.han.nl>
2024-09-22 20:08:17 +0200

    utils

--- tytorch/utils.py
+++ tytorch/utils.py
@@ -1,13 +1,13 @@
 import torch
-
+from loguru import logger
 def get_device() -> str:
     if torch.backends.mps.is_available() and torch.backends.mps.is_built():
         device = torch.device("mps")
-        print("Using MPS")
+        logger.info("Using MPS")
     elif torch.cuda.is_available():
         device = "cuda:0"
-        print("using cuda")
+        logger.info("using cuda")
     else:
         device = "cpu"
-        print("using cpu")
+        logger.info("using cpu")
     return device
